51fe02481eee45a9c007db2d571af71adaed488a
DeepBluet <1360034559@qq.com>
2020-02-24 03:30:59 +0800

    add properties

--- src/main/java/life/peng/community/controller/AuthorizeController.java
+++ src/main/java/life/peng/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.peng.community.dto.AccesTokensDTO;
 import life.peng.community.dto.GithubUser;
 import life.peng.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,14 @@ public class AuthorizeController {
     @Autowired//自动将写好的实例化对象加载到IOC容器中
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    //value将会读取properties文件中对应的值,从而实现业务层的封装
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     //RequestMapping的作用是请求路径的跳转,相当于文件系统,Url唯一
     //RequestBody会将这个方法返回的数据通过IO流写入到浏览器
@@ -24,10 +33,10 @@ public class AuthorizeController {
                           //@RequestParam 获取请求参数的值
     {
         AccesTokensDTO accesTokensDTO=new AccesTokensDTO();
-        accesTokensDTO.setClient_id("6ec6304bbb35b05f0194");
-        accesTokensDTO.setClient_secret("c4661c64b6e9a79a887ca694d88b45de9ccdba70");
+        accesTokensDTO.setClient_id(clientId);
+        accesTokensDTO.setClient_secret(clientSecret);
         accesTokensDTO.setCode(code);
-        accesTokensDTO.setRedirect_url("http://localhost:8887/callback");
+        accesTokensDTO.setRedirect_url(redirectUri);
         accesTokensDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accesTokensDTO);
         GithubUser user = githubProvider.getuser(accessToken);
