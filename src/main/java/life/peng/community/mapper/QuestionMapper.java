3df7fcdc8e26c6cf67929c1faaefdc70f1f0a147
DeepBluet <1360034559@qq.com>
2020-02-26 16:55:01 +0800

    modified publish

---
+++ src/main/java/life/peng/community/mapper/QuestionMapper.java
@@ -0,0 +1,15 @@
+package life.peng.community.mapper;
+
+import life.peng.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface QuestionMapper {
+
+    //插入语句一定要正确，否则服务器会报提交异常，则无法跳转
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create(Question question);
+}
